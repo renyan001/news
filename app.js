3e951b055ceff3cf958ec2c6ec229a65d94b56f7
renyan001 <370778337@qq.com>
2018-12-19 17:14:08 +0800

    注册

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
 // 导包
 const express = require('express');
 const router = require('./router');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const MySQLStore = require('express-mysql-session')(session);
+const options = {
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: 'root',
+  database: 'news'
+};
+
+const sessionStore = new MySQLStore(options);
 // 配置
 const app = express();
 // 配置静态资源
 app.use('/public',express.static('./public'));
 // 配置第三方包
 app.use('/node_modules',express.static('./node_modules'));
+
 app.engine('html',require('express-art-template'));
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(bodyParser.json());
+
+app.use(session({
+  key: 'session_cookie_name',
+  secret: 'session_cookie_secret',
+  store: sessionStore,
+  resave: false,
+  saveUninitialized: false
+}));
+
 // 使用路由
 app.use(router);
 // 监听端口
--- router.js
+++ router.js
@@ -1,9 +1,22 @@
 // 导包
 const express = require('express');
 const c_user = require('./controllers/c_user');
+const c_topic = require('./controllers/c_topic');
 // 配置
 const router = express.Router();
 // 使用路由
-router.get('/signin', c_user.showSignin);
+router.get('/signin', c_user.showSignin)
+      .post('/signin', c_user.handleSignin)
+      .get('/', c_topic.showTopicList)
+      .get('/topic/create', c_topic.showCreateTopic)
+      .post('/createTopic', c_topic.handleCreateTopic)
+      .get('/signout', c_user.handleSignout)
+      // .get('/detail/topic', c_topic.showTopicDetail);
+      .get('/detail/topic/:topicID', c_topic.showTopicDetail)
+      .get('/topic/:topicID/delete', c_topic.handleDeleTopic)
+      .get('/topic/:topicID/edit', c_topic.showEditTopic)
+      .post('/edit/topic/:topicID', c_topic.handleEditTopic)
+      .get('/signup', c_user.showSignup)
+      .post('/signup',c_user.handleSignup);
 // 导出
 module.exports = router;
